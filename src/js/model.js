32cdfedabd12558c6f2003a55d07fa07d150392e
Filonenko Dmytro <filonenkodmytrodm@gmail.com>
2024-10-08 11:32:02 +0300

    add icon to own recipes

--- src/js/model.js
+++ src/js/model.js
@@ -29,7 +29,7 @@ const createRecipeObject = function (data) {
 
 export const laodRecipe = async function (id) {
   try {
-    const data = await AJAX(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some((bookmark) => bookmark.id === id))
@@ -43,7 +43,7 @@ export const laodRecipe = async function (id) {
 export const loadSearchResult = async function (query) {
   try {
     state.search.query = query;
-    const data = await AJAX(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
     console.log(data);
 
     state.search.results = data.data.recipes.map((rec) => {
@@ -52,6 +52,7 @@ export const loadSearchResult = async function (query) {
         title: rec.title,
         publisher: rec.publisher,
         image: rec.image_url,
+        ...(rec.key && { key: rec.key }),
       };
     });
     console.log(state.search.results);
@@ -115,7 +116,7 @@ export const uploadRecipe = async function (newRecipe) {
     const ingredients = Object.entries(newRecipe)
       .filter((entry) => entry[0].startsWith("ingredient") && entry[1] !== "")
       .map((ing) => {
-        const ingArr = ing[1].replaceAll(" ", "").split(",");
+        const ingArr = ing[1].split(",").map((el) => el.trim());
         if (ingArr.length !== 3)
           throw new Error(
             "Wrong ingredient format! Please use the correct format :)"
--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -1,4 +1,5 @@
 import View from "./View";
+import icons from "url:../../img/icons.svg";
 
 class PreviewView extends View {
   _parentElement = "";
@@ -6,22 +7,29 @@ class PreviewView extends View {
   _generateMarkup() {
     const id = window.location.hash.slice(1);
     return `
-          <li class="preview">
-            <a
-              href="#${this._data.id}"
-              class="preview__link ${
-                this._data.id === id ? "preview__link--active" : ""
-              }"
-            >
-              <figure class="preview__fig">
-                <img src="${this._data.image}" alt="${this._data.title}" />
-              </figure>
-              <div class="preview__data">
-                <h4 class="preview__title">${this._data.title}</h4>
-                <p class="preview__publisher">${this._data.publisher}</p>
-              </div>
-            </a>
-          </li>
+      <li class="preview">
+        <a
+          href="#${this._data.id}"
+          class="preview__link ${
+            this._data.id === id ? "preview__link--active" : ""
+          }"
+        >
+          <figure class="preview__fig">
+            <img src="${this._data.image}" alt="${this._data.title}" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${this._data.title}</h4>
+            <p class="preview__publisher">${this._data.publisher}</p>
+            <div class="preview__user-generated ${
+              this._data.key ? "" : "hidden"
+            }">
+              <svg>
+                <use href="${icons}#icon-user"></use>
+              </svg>
+            </div>
+          </div>
+        </a>
+      </li>
       `;
   }
 }
